70cdfe3196898dc7e2c38c49a70642219db7263b
Aksh <akashvishwakarma111111@gmail.com>
2023-09-26 08:04:48 +0530

    Initial commit

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h4>
+            This is About Page
+        </h4>
+    </div>
+  )
+}
+
+export default about
---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h4>
+            This is Contact Page
+        </h4>
+    </div>
+  )
+}
+
+export default Contact
